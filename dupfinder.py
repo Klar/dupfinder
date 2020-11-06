2eefcaeb6d2234d19313b64c0be209bdfe3d4661
klar <a@pxd.ch>
2020-11-06 10:53:52 +0100

    fixed move() function

--- dupfinder.py
+++ dupfinder.py
@@ -5,52 +5,44 @@ import hashlib
 import os
 
 #set directory to check for duplicates
-directory = '/home/user/Desktop/folder'
-movedir= directory + '/duplicates/'
-
-foldersave = 2 
+directory = '/run/media/link/extHD/webcet'
+movedir= directory + '/duplicates'
 
 #get root and filename from given directory
 def get_filepath(dir):
   files = []
-  print "get_filepath(" + dir + ")" 
+  print "get_filepath(" + dir + ")"
   for root, dirs, filenames in os.walk(dir):
       for filename in filenames:
           files.append([root,filename])
   return files
 
-def move():
-      rootSplit = root.split("/")
-      myList = ""
-      for count in range(foldersave + 1):
-        if count == 0:
-          continue
-        myList = rootSplit[-count] + "/" + myList
+def move(root, filename):
+      folderpath = root.replace(directory,'')
 
-      movingdir = movedir + myList
-
+      movingdir = movedir + folderpath 
       # create new duplicate directory
       try:
         os.makedirs(movingdir)
       except:
         print()
 
-      os.rename(root + "/" + filename, movingdir + filename)
-      print "duplicate move to: " + movingdir
+      os.rename(root + "/" + filename, movingdir + "/" + filename)
+      print "duplicate moved to: " + movingdir + "/" + filename 
 
 #get root, filename and hash from given directory
 def get_hash_do_action(files):
   hashfiles = []
   hashes = []
-  print "get_hash()" 
+  print "get_hash()"
   for root,filename in files:
     image_file = open(root + "/" + filename).read()
     hash = hashlib.md5(image_file).hexdigest()
     hashfiles.append([root,filename,hash])
-    if hash in hashes:
 
+    if hash in hashes:
       #os.remove(root + "/" + filename)
-      move()
+      move(root, filename)
 
     else:
       hashes.append(hash)
